236c88f25b70f169d9533e3b8ab4cd100918d04d
Kun Lin Liu <78029945+kunlin1013@users.noreply.github.com>
2023-12-25 21:53:31 +0800

    Train with a more diverse set of data augmentation

--- Train_DLinkNet.py
+++ Train_DLinkNet.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt 
 import numpy as np
 import tensorflow as tf
@@ -14,6 +16,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import CSVLogger
 from segmentation_models.metrics import iou_score
 from segmentation_models.losses import bce_dice_loss
+
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
@@ -33,30 +36,32 @@ def get_project_dir():
 
     return project_directory
 
+
 # Set training hyperparameters 
 BATCH_SIZE = 16
 LEARNING_RATE = 0.0001
-LEARNING_RATE_DECAY_FACTOR = 0.1
-LEARNING_RATE_DECAY_PATIENCE = 2
-EARLY_STOPPING_PATIENCE = 10
+LEARNING_RATE_DECAY_FACTOR = 0.08
+LEARNING_RATE_DECAY_PATIENCE = 3
+EARLY_STOPPING_PATIENCE = 8
 EPOCHS = 60
 
 if __name__ == '__main__':
-
-    # 10W images from synthetic dataset 
+    # Load training data and divide into two parts: training set and testing set 
     split_ratio = 0.7
-    data = [str(i) for i in range(40000, 140000)]
-    random.shuffle(data)
+    with open(f'{get_project_dir()}\\train.txt', 'r') as file:
+        data = file.read().split("\n")[:-1]
+        random.shuffle(data)
+
     idx = int(np.floor(len(data) * (split_ratio)))
     train_list = data[0:idx]
     val_list = data[idx:-1]
     np.save('val_list.npy', np.array(val_list))
-    
-    # Load synthetic data 
-    json_file_path = f"{get_project_dir()}\\syn_data_dict.json"
+
+    # Load json file
+    json_file_path = f"{get_project_dir()}\\data_dict.json"
     with open(json_file_path, 'r') as json_file:
         data_json = json.load(json_file)
-    
+
     # Use DataGenerator to generate train batch and val batch
     train, train_count = DataGenerator_train(dir='train', img_list=train_list, data_json=data_json, IsAugmentation=True, batch_size=BATCH_SIZE)
     val, val_count  = DataGenerator_train(dir='val', img_list=val_list, data_json=data_json, IsAugmentation=False, batch_size=BATCH_SIZE)
@@ -72,12 +77,12 @@ if __name__ == '__main__':
                                    patience=EARLY_STOPPING_PATIENCE, 
                                    verbose=1, 
                                    mode='min') 
-    
+
     csv_logger = CSVLogger('./training.log')
-    
+
     filepath="DLinkNet-weights-improvement-{epoch:02d}-{val_loss:.3f}.h5"
     checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-    
+
     callbacks_list = [checkpoint, Reduce, early_stopping, csv_logger] 
 
     # Load model and set complie
@@ -91,7 +96,8 @@ if __name__ == '__main__':
     val_dataset = val.repeat()
     train_steps = train_count // BATCH_SIZE
     val_steps = val_count // BATCH_SIZE
-    
+
+
     # Train model
     history = net_final.fit(train_dataset,
                             steps_per_epoch=train_steps,
@@ -100,7 +106,8 @@ if __name__ == '__main__':
                             epochs=EPOCHS,
                             callbacks=callbacks_list
                             )
-    
+
+
     # Save acc/loss figure
     plt.figure(figsize=(10, 6))
     plt.plot(history.history['iou_score'], label='iou_score')
@@ -109,10 +116,10 @@ if __name__ == '__main__':
     plt.ylabel('iou')
     # plt.ylim([0, 1])
     plt.legend(loc='lower right')
+
     # test_loss, test_acc = net_final.evaluate([X_test],Y_test, verbose=2)
     print('iou_score=',history.history['iou_score'][-1],"   ","val_iou_score=",history.history['val_iou_score'][-1])
     plt.savefig(r'.\DLinkNet_iou.png')
-    
     plt.figure(figsize=(10, 6))
     plt.plot(history.history['loss'], label='loss')
     plt.plot(history.history['val_loss'], label='val_loss')
@@ -123,4 +130,5 @@ if __name__ == '__main__':
     # test_loss, test_acc = net_final.evaluate([X_test],Y_test, verbose=2)
     print('loss=',history.history['loss'][-1],"   ","val_loss=",history.history['val_loss'][-1])
     plt.savefig(r'.\DLinkNet_loss.png')
+
 tf.config.experimental.set_memory_growth(gpus[0],True)
